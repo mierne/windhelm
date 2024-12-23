e2e2e29eef81d379a3f69c0b11bf260cb459c0bf
mierne <ahoy@mierne.net>
2024-12-23 18:49:39 -0500

    update exploration_engine.bat
    - Fix typos
    - Fix crashes
    - Add Windhelm Exterior
    - Add Windhelm Exterior encounter

--- data/Exploration Engine/exploration_engine.bat
+++ data/Exploration Engine/exploration_engine.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 TITLE (WINDHELM) - EXPLORATION ENGINE ^| %player.name% the %player.class%!
-REM Exploration Engine Pre-Alpha wee-0.1.0-241221-PA1-GU0
+REM Exploration Engine Pre-Alpha wee-0.1.0-241223-PA1-GU0
 
 :MAIN
 MODE con: cols=105 lines=19
 CLS
 ECHO.
-REM TYPE "%winLoc%\PATH\TO\ASCII\ART"
+TYPE "%winLoc%\data\assets\ui\exploration.txt"
 ECHO.
 ECHO Where is it you wish to go, %player.name%?
 ECHO +-------------------------------------------------------------------------------------------------------+
@@ -47,11 +47,61 @@ IF %EE% LEQ 50 (
     SET displayMessage=You didn't find anything of interest.
     GOTO :VENTURE_IRIDESCENT_FOREST
 )
-
 :IFOR_CHALLENGE_AREA_BOSS
 IF %player.level% GEQ 20 (
     SET currentEnemy="Abyssal Guardian"
+    CALL "%winLoc%\data\Combat Engine\scripts\evie.bat"
+) ELSE (
+    SET displayMessage=Your level is too low for this encounter.
+    GOTO :VENTURE_IRIDESCENT_FOREST
 )
+
+:VENTURE_WINDHELM_EXTERIOR
+MODE con: cols=105 lines=19
+CLS
+ECHO.
+REM TYPE "%winLoc%\PATH\TO\ASCII\ART"
+ECHO.
+ECHO What is it you wish to do, %player.name%?
+ECHO +-------------------------------------------------------------------------------------------------------+
+ECHO ^| HP: %player.health% ^| XP: %player.xp%/%player.xp_required% ^| LUNIS: %player.coins% ^| AT: %player.damage% ^| AM: %player.armor% ^| ST: %player.stamina% ^| MG: %player.magicka%
+ECHO +-------------------------------------------------------------------------------------------------------+
+ECHO ^| [1 / WANDER ] ^| [2 / TRAVELING MERCHANT ] ^| [Q / BACK ] ^| %displayMessage%
+ECHO +-------------------------------------------------------------------------------------------------------+
+CHOICE /C 12Q /N /M ">"
+IF ERRORLEVEL 3 GOTO :MAIN
+IF ERRORLEVEl 2 GOTO :WE_TRAVELING_MERCHANT
+IF ERRORLEVEL 1 GOTO :WE_WANDER
+
+:WE_WANDER
+SET /A  WE=%RANDOM% %%50
+IF %WE% LEQ 15 (
+    REM ENCOUNTER PLACEHOLDER NPC (QUESTGIVER)
+    GOTO :WE_WANDER_ENCOUNTER_1
+) ELSE (
+    SET displayMessage=You didn't find anything of interest.
+    GOTO :VENTURE_WINDHELM_EXTERIOR
+)
+
+:WE_WANDER_ENCOUNTER_1
+MODE con: cols=105 lines=19
+CLS
+ECHO.
+REM TYPE "%winLoc%\PATH\TO\ASCII\ART"
+ECHO.
+ECHO You see someone with what appears to be a polearm closely following another.
+ECHO +-------------------------------------------------------------------------------------------------------+
+ECHO ^| HP: %player.health% ^| XP: %player.xp%/%player.xp_required% ^| LUNIS: %player.coins% ^| AT: %player.damage% ^| AM: %player.armor% ^| ST: %player.stamina% ^| MG: %player.magicka%
+ECHO +-------------------------------------------------------------------------------------------------------+
+ECHO ^| [1 / APPROACH ] ^| [Q / BACK ] ^| %displayMessage%
+ECHO +-------------------------------------------------------------------------------------------------------+
+CHOICE /C 1Q /N /M ">"
+IF ERRORLEVEL 3 GOTO :MAIN
+IF ERRORLEVEL 1 GOTO :WEWE1_APPROACH_INTERACTION
+
+:WEWE1_APPROACH_INTERACTION
+SET displayMessage=NOT IMPLEMENTED.
+GOTO :MAIN
 
 REM Save Player progress before quitting Exploration Engine.
 :AUTOSAVE
