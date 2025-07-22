f6697fd0e9b15a456a1352a230fa40e8c5e9481d
Valley <ahoy@mierne.net>
2025-07-21 22:06:39 -0400

    update Error Handler.bat
    - Add support for Pulse Engine
    - Remove outdated errors

--- data/functions/Error Handler.bat
+++ data/functions/Error Handler.bat
@@ -1,75 +1,42 @@
-REM Just handling the errors!
-
-REM Check error type
-IF %errorType% == EnemyType (
-    GOTO :errorType_enemy
-) ELSE IF %errorType% == checkTime (
-    GOTO :errorType_checkTime
-) ELSE IF %errorType% == attributeSkill (
-    GOTO :errorType_attributeSkill
-) ELSE IF %errorType% == encounterError (
-    GOTO :errorType_encounterError
-) ELSE IF %errorType% == LastLocal (
-    GOTO :errorType_lastLocal
-) ELSE IF %errorType% == invalidString (
-    GOTO :errorType_invalidString
+IF %errorType% == sublevel (
+    GOTO :errorType_SUBLEVEL
+) ELSE IF %errorType% == areazone (
+    GOTO :errorType_AREAZONE
 ) ELSE (
-    REM ...an error, inside of Error Handler??? Unthinkable!
-    SET errorType=unknownError
-    ECHO Error handler encountered an unhandled error. Check for logs from other scripts! >> ErrorHandler.log
-    EXIT
+    GOTO :errorType_ERRORHANDLER
 )
 
-REM Display the "Enemy Type" error.
-:errorType_enemy
-ECHO 
-
+:errorType_SUBLEVEL
 CLS
 ECHO.
-ECHO ERROR! Error Type: EnemyType.
-ECHO =============================
-ECHO This could mean:
-ECHO The "currentEnemy" variable was left blank, or is unrecognized.
+ECHO Windhelm has encountered an error. The following information is known:
+ECHO    Calling Script: %callingScript%
+ECHO    Caused By: %errorCause%
+ECHO    On line: %scriptLine%
+ECHO What can you do?:
+ECHO There is likely nothing the user can do to fix this issue. Please open an issue on the GitHub page.
 PAUSE
 EXIT
 
-REM Display the "Attribute - Skill" error.
-:errorType_attributeSkill
+:errorType_AREAZONE
 CLS
 ECHO.
-ECHO ERROR! Error Type: attributeSkill.
-ECHO Wuh-oh, Windhelm ran into a problem and can't continue!
-ECHO What'd you do this time?
-ECHO =======================================================
-ECHO This error could mean:
-ECHO The Player has modified their save, attempting to adjust Skill Levels to an undefined integer.
-ECHO This... this is the only way to cause this error. Stop messing with my shit!
+ECHO Windhelm has encountered an error. The following information is known:
+ECHO    Calling Script: %callingScript%
+ECHO    Caused By: %errorCause%
+ECHO    On line: %scriptLine%
+ECHO What can you do?:
+ECHO There is likely nothing the user can do to fix this issue. Please open an issue on the GitHub page.
 PAUSE
 EXIT
 
-:errorType_encounterError
+:errorType_ERRORHANDLER
 CLS
 ECHO.
-ECHO Error Encountered - Windhelm has stopped.
-ECHO Error Type: %errorType%
-ECHO =======================================================
-ECHO This could be caused by a corrupted script or save file.
-ECHO If this repeats, email nope@donotemailme.fuckoff.com
+ECHO Windhelm has encountered an error. The following information is known:
+ECHO    Calling Script: Error Handler.bat
+ECHO    Caused By: Invalid errorType string
+ECHO What can you do?:
+ECHO There is likely nothing the user can do to fix this issue. Please open an issue on the GitHub page.
 PAUSE
-EXIT
-
-:errorType_lastLocal
-CLS
-ECHO.
-ECHO Error Encountered - Windhelm has stopped.
-ECHO Error Type: %errorType%
-echo %ll%
-ECHO =======================================================
-ECHO Corrupted or modified save file.
-ECHO If this repeats, email nope@donotemailme.fuckoff.com
-PAUSE
-EXIT
-
-:errorType_invalidString
-ECHO Combat Engine.bat: LINE %errorLineCE%. errorType? %errorType%. Occured on %DATE%,%TIME%. Critical? Yes. >> winLog.txt
 EXIT
