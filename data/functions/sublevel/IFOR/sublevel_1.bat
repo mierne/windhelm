c45991d8d665c2e55cd62ccb5ae4db15c146de0f
Kat <ahoy@mierne.net>
2025-12-28 18:27:57 -0500

    update sublevel_1.bat
    - Add remaining options
    - Add remaining scenes

--- data/functions/sublevel/IFOR/sublevel_1.bat
+++ data/functions/sublevel/IFOR/sublevel_1.bat
@@ -1,5 +1,5 @@
 title Windhelm - %windhelm.ut% ^| Iridescent Forest: The Grove ^| %player.name% the %player.race% %player.class%
-mode con: cols=125 lines=23
+mode con: cols=125 lines=20
 set displayMessage=...
 rem Iridescent Forest Sublevel 1 Deep Exploration Mode - The Grove
 
@@ -192,6 +192,169 @@ echo +--------------------------------------------------------------------------
 echo ^| [1 / Continue ]
 set /p ch="> "
 if /i "%ch%" == "1" goto :scene_1
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_plants_back_up
+set rtm=scene_1_examine_plants_back_up
+set player.ifor_level_1_location=scene_1_examine_plants_back_up
+set player.scene_1_examine_plants=complete
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| You back up slowly in an attempt to avoid the plant's defenses, but you're too slow. The noxious gas knocks you to your
+echo ^| feet, but you managed to crawl away in time before it could do more damage.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Continue ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_barrels
+if %player.scene_1_examine_barrels% == complete (
+    set displayMessage=You've already done this.
+    goto :%rtm%
+)
+set rtm=scene_1_examine_barrels
+set player.ifor_level_1_location=scene_1_examine_barrels
+set player.scene_1_examine_barrels=complete
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| As you lift the lid from the barrels, you're greeted with... an empty barrel. There remains crumbs from a long gone food.
+echo ^| You wonder if perhaps the wagon was carrying this barrel and if they had fallen off.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Continue ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_wagon
+if %player.scene_1_examine_wagon% == complete (
+    set displayMessage=You've already done this.
+    goto :%rtm%
+)
+set rtm=scene_1_examine_wagon
+set player.ifor_level_1_location=scene_1_examine_wagon
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| As you inch closer to the horses and wagon, one of the horses notices you and spooks, bolting before you can get closer.
+echo ^| You notice a chest in the back of the wagon, it might contain something useful...
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Inspect the area (INTELLIGENCE)] ^| [2 / Check the chest ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1_examine_wagon_inspect_area
+if /i "%ch%" == "2" goto :scene_1_examine_wagon_check_chest
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_wagon_inspect_area
+if %player.s1_exwag_inspect_area% == complete (
+    set displayMessage=You've already done this.
+    goto :%rtm%
+)
+set rtm=scene_1_examine_wagon_inspect_area
+set player.ifor_level_1_location=scene_1_examine_wagon_inspect_area
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| Roll with INTELLIGENCE? Your current intelligence skill is: %player.skill_intelligence%
+echo ^| You must roll a 10 or higher to succeed this check.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Roll 1d20 +%player.skill_intelligence%] ^| [2 / Think again... ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1_examine_wagon_inspect_area_roll
+if /i "%ch%" == "2" goto :scene_1_examine_wagon
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+
+:scene_1_examine_wagon_inspect_area_roll
+call "%winLoc%\data\functions\roll.bat"
+set /a dieroll=%dieroll% +%player.skill_intelligence%
+if %dieroll% geq 10 (
+    set displayMessage=You rolled a %dieroll%, success!
+    goto :scene_1_examine_wagon_inspect_area_roll_success
+) else (
+    set displayMessage=You rolled a %dieroll%, failure!
+   goto :scene_1_examine_wagon_inspect_area_roll_failure
+)
+
+:scene_1_examine_wagon_inspect_area_roll_success
+set rtm=scene_1_examine_wagon_inspect_area_roll_success
+set player.ifor_level_1_location=scene_1_examine_wagon_inspect_area_roll_success
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%^^!
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| You look closely at the surrounding area and notice an extremely faint footprint. You look further down the trail it
+echo ^| created and notice a pool of blood next to some tall, thick grass.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Continue ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1_examine_wagon_inspect_area_roll_success_follow_trail
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_wagon_inspect_area_roll_success_follow_trail
+set rtm=scene_1_examine_wagon_inspect_area_roll_success_follow_trail
+set player.ifor_level_1_location=scene_1_examine_wagon_inspect_area_roll_success_follow_trail
+set player.scene_1_examine_wagon=complete
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%^^!
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| You carefully follow the trail up to the grass. You hesitate a moment before pushing aside the grass to peer inside.
+echo ^| Among the tall grass lay a corpse, filled with arrows. Whoever this was had been ambushed and attacked.
+echo ^| Beside them lay a bloody note and a longsword.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Contine ] ^| [2 / Collect items ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1_examine_wagon_inspect_area_roll_success_follow_trail_collect
+if /i "%ch%" == "2" goto :scene_1_examine_wagon_inspect_area_roll_success_follow_trail_collect
+if /i "%ch%" == "S" goto :save_game
+if /i "%ch%" == "E" goto :autosave
+
+:scene_1_examine_wagon_inspect_area_roll_success_follow_trail_collect
+set displayMessage=Collected 1 longsword and 1 bloody note.
+set /a player.item_long_sword_owned=!player.item_long_sword_owned! +1
+rem Inventory system doesn't support unique items yet.
+goto :scene_1
+
+:scene_1_examine_wagon_inspect_area_roll_failure
+set rtm=scene_1_examine_wagon_inspect_area_roll_success
+set player.ifor_level_1_location=scene_1_examine_wagon_inspect_area_roll_success
+set player.s1_exwag_inspect_area=complete
+cls
+echo.
+type "%winLoc%\data\assets\ui\the_grove.txt"
+echo.
+echo %displayMessage%^^!
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| You look closely at the surrounding area but notice nothing out of the ordinary.
+echo +---------------------------------------------------------------------------------------------------------------------------+
+echo ^| [1 / Continue ]
+set /p ch="> "
+if /i "%ch%" == "1" goto :scene_1_examine_wagon
 if /i "%ch%" == "S" goto :save_game
 if /i "%ch%" == "E" goto :autosave
 
